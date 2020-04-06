907bc67869be03757e96217fd41d2f6640738420
20eharar <62620840+20eharar@users.noreply.github.com>
2020-04-06 11:30:14 -0400

    Add files via upload

--- animation/ArcadeDemo.java
+++ animation/ArcadeDemo.java
@@ -11,16 +11,15 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 /**
- * Class ArcadeDemo
- * This class contains demos of many of the things you might
+ * Class ArcadeDemo This class contains demos of many of the things you might
  * want to use to make an animated arcade game.
- * 
- * Adapted from the AppletAE demo from years past. 
+ *
+ * Adapted from the AppletAE demo from years past.
  */
-
 import gameObjects.Item;
 import gameObjects.Player;
 import gameObjects.Room;
+import javafx.scene.input.KeyCode;
 import rooms.KamiRoomOne;
 import rooms.MultiButtonRoom;
 import rooms.RoomOne;
@@ -30,148 +29,158 @@ import rooms.RoomTwo;
 import rooms.RoomWithStuff;
 import rooms.RoomZero;
 
-
-public class ArcadeDemo extends AnimationPanel 
-{
+public class ArcadeDemo extends AnimationPanel {
 
     //Constants
     //-------------------------------------------------------
-
     //Instance Variables
     //-------------------------------------------------------
-    private Player player; 
+    private Player player;
     private ArrayList<Room> rooms;
-    private int currentRoom; 
-    
+    private int currentRoom;
+
     private boolean waitMode;
     private int waitBeforeNextRoomTimer;
     private int totalTime;
     private int roomTime;
+    private boolean editorMode = true;
+    private RoomEditor r = new RoomEditor();
 
     //Constructor
     //-------------------------------------------------------
-    public ArcadeDemo()
-    {   //Enter the name and width and height.  
+    public ArcadeDemo() {   //Enter the name and width and height.  
         super("Escape Room 2020", 640, 480);
         player = new Player("Demo");
         rooms = new ArrayList<>();
         addRoomsToList();
         currentRoom = 0;
-        
         waitMode = false;
         waitBeforeNextRoomTimer = 0;
-        totalTime=0;
-        roomTime=0;
+        totalTime = 0;
+        roomTime = 0;
     }
-    
-    public void addRoomsToList()
-    {
-    	rooms.add(new KamiRoomOne());
-    	rooms.add(new RoomThree());
+
+    public void addRoomsToList() {
+        rooms.add(new KamiRoomOne());
+        rooms.add(new RoomThree());
         rooms.add(new RoomTwo());
         rooms.add(new RoomZero());
         rooms.add(new MultiButtonRoom());
         rooms.add(new MultiButtonRoom());
         rooms.add(new RoomWithStuff());
         rooms.add(new RoomOne());
-        
+
     }
-       
+
     //The renderFrame method is the one which is called each time a frame is drawn.
     //-------------------------------------------------------
-    protected Graphics renderFrame(Graphics g) 
-    {
-        //Draw a square that is stationary on the screen.
-       // g.setColor(Color.BLUE);
-        //g.fillRect(220,120,50,50);
-        
-        //Draw a circle that follows the mouse.
-        g.setColor(Color.BLACK);
-        g.fillOval(mouseX-10, mouseY-10, 20,20);
-        
-        rooms.get(currentRoom).draw(g,this);
-        
-        if (rooms.get(currentRoom).isDone() && !waitMode) 
-        {   
-            waitMode = true;
-            waitBeforeNextRoomTimer = 200;
-        }
-        if(waitMode)
-        {
-            if(waitBeforeNextRoomTimer > 0)
-            {
-                waitBeforeNextRoomTimer--;
-                g.drawString("You Have Escaped!", 250, 150);
+    protected Graphics renderFrame(Graphics g) {
+        if (!editorMode) {
+            //Draw a square that is stationary on the screen.
+            // g.setColor(Color.BLUE);
+            //g.fillRect(220,120,50,50);
+
+            //Draw a circle that follows the mouse.
+            g.setColor(Color.BLACK);
+            g.fillOval(mouseX - 10, mouseY - 10, 20, 20);
+
+            rooms.get(currentRoom).draw(g, this);
+
+            if (rooms.get(currentRoom).isDone() && !waitMode) {
+                waitMode = true;
+                waitBeforeNextRoomTimer = 200;
             }
-            else
-            {
-                waitMode = false;
-                if(currentRoom < rooms.size()-1)
-                {
-                    currentRoom++;
-                    roomTime=0;
+            if (waitMode) {
+                if (waitBeforeNextRoomTimer > 0) {
+                    waitBeforeNextRoomTimer--;
+                    g.drawString("You Have Escaped!", 250, 150);
+                } else {
+                    waitMode = false;
+                    if (currentRoom < rooms.size() - 1) {
+                        currentRoom++;
+                        roomTime = 0;
+                    } else {
+                        System.out.println("You Have Escaped ALL the rooms!");
+                    }
                 }
-                else
-                    System.out.println("You Have Escaped ALL the rooms!");
+
             }
-            
+
+            //General Text (Draw this last to make sure it's on top.)
+            g.setColor(Color.BLACK);
+            g.drawString("CurrentRoom = " + currentRoom, 10, 12);
+            g.drawString("f#" + frameNumber, 150, 12);
+            g.drawString("Total Time: " + totalTime / 60, 500, 20);
+            g.drawString("Room Time: " + roomTime / 60, 500, 32);
+            roomTime++;
+            totalTime++;
+        } else {
+            r.drawEditor(g, this, mouseX, mouseY);
         }
-        
-        //General Text (Draw this last to make sure it's on top.)
-        g.setColor(Color.BLACK);
-        g.drawString("CurrentRoom = "+currentRoom, 10, 12);
-        g.drawString("f#"+frameNumber, 150, 12);
-        g.drawString("Total Time: "+totalTime/60, 500, 20);
-        g.drawString("Room Time: "+roomTime/60, 500, 32);
-        roomTime++;
-        totalTime++;
         return g;
     }//--end of renderFrame method--
-    
+
     //-------------------------------------------------------
     //Respond to Mouse Events
     //-------------------------------------------------------
-    public void mouseClicked(MouseEvent e)  
-    { 
+    public void mouseClicked(MouseEvent e) {
+        r.click();
         Point clickPoint = e.getPoint();
         rooms.get(currentRoom).onClick(clickPoint, player);
         rooms.get(currentRoom).onClickGeneric(clickPoint, player);
     }
-    public void mouseDragged(MouseEvent e) 
-    {
-    	Point clickPoint = e.getPoint();
+
+    public void mouseDragged(MouseEvent e) {
+        Point clickPoint = e.getPoint();
         rooms.get(currentRoom).onDrag(clickPoint, player);
-    	
+
     }
-    public void mouseReleased(MouseEvent e) 
-    {
-    	Point clickPoint = e.getPoint();
+
+    public void mouseReleased(MouseEvent e) {
+        Point clickPoint = e.getPoint();
         rooms.get(currentRoom).onMouseRelease(clickPoint, player);
-    	
+
     }
-    
+
     //-------------------------------------------------------
     //Respond to Keyboard Events
     //-------------------------------------------------------
-    public void keyTyped(KeyEvent e) 
-    {
+    public void keyTyped(KeyEvent e) {
         char c = e.getKeyChar();
         rooms.get(currentRoom).onKey(c, player);
     }
-    
-    public void keyPressed(KeyEvent e)
-    {
+
+    public void keyPressed(KeyEvent e) {
         int v = e.getKeyCode();
-        
+        arrowKeys(v);
+
     }
 
-    public void keyReleased(KeyEvent e)
-    {
+    public void keyReleased(KeyEvent e) {
         int v = e.getKeyCode();
-        
+
     }
-    
-    
+
+    public void arrowKeys(int v) {
+        boolean left = false;
+        boolean right = false;
+        boolean up = false;
+        boolean down = false;
+        if (v == KeyEvent.VK_UP) {
+            up = true;
+        }
+        if (v == KeyEvent.VK_DOWN) {
+            down = true;
+        }
+        if (v == KeyEvent.VK_LEFT) {
+            left = true;
+        }
+        if (v == KeyEvent.VK_RIGHT) {
+            right = true;
+        }
+        r.offset(left, right, up, down);
+    }
+
     //-------------------------------------------------------
     //Initialize Graphics
     //-------------------------------------------------------
@@ -182,18 +191,17 @@ public class ArcadeDemo extends AnimationPanel
  *  2.  Actually make the image and store it in the same directory as the code.
  *  3.  Add a line into the initGraphics() function to load the file. 
 //-----------------------------------------------------------------------*/
-    Image ballImage;        
+    Image ballImage;
     Image starImage;
-    
-    public void initGraphics() 
-    {      
+
+    public void initGraphics() {
         Toolkit toolkit = Toolkit.getDefaultToolkit();
-        
+
         Item.loadImages(toolkit);
 //        ballImage = toolkit.getImage("ball.gif");
 //        starImage = toolkit.getImage("star.gif");
     } //--end of initGraphics()--
-    
+
     //-------------------------------------------------------
     //Initialize Sounds
     //-------------------------------------------------------
@@ -207,9 +215,8 @@ public class ArcadeDemo extends AnimationPanel
 //-----------------------------------------------------------------------*/
     AudioClip themeMusic;
     AudioClip bellSound;
-    
-    public void initMusic() 
-    {
+
+    public void initMusic() {
     }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
---
+++ animation/RoomEditor.java
@@ -0,0 +1,160 @@
+package animation;
+
+import gameObjects.Item;
+import gameObjects.Room;
+import items.BasicDoor;
+import items.ColorCombo;
+import items.CombinationSafe;
+import items.Key;
+import items.LockedDoor;
+import items.NamedButton;
+import items.NumberedDoor;
+import items.Paper;
+import items.SimpleButton;
+import items.SimpleButtonBordered;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.image.ImageObserver;
+import java.util.ArrayList;
+import rooms.RoomOne;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author rickyehara
+ */
+public class RoomEditor {
+
+    ArrayList<Item> staticitems = new ArrayList<>();
+    ArrayList<Item> items = new ArrayList<>();
+    ArrayList<Boolean> buttonOver = new ArrayList();
+    ArrayList<Rectangle> buttons = new ArrayList();
+    ArrayList<Item> placed = new ArrayList();
+    Item selected;
+    int x = 0;
+    int y = 0;
+    int xoffset = 0;
+    int yoffset = 0;
+    String s = "placeholder";
+    boolean menu = true;
+    Room r;// = new RoomOne();
+
+    public RoomEditor(Room rin) {
+        r = rin;
+        initializeItems();
+    }
+
+    public RoomEditor() {
+        initializeItems();
+    }
+
+    private void initializeItems() {
+        items.add(new BasicDoor());
+        items.add(new CombinationSafe());
+        //items.add(new KamiPuzzle());
+        items.add(new LockedDoor());
+        items.add(new Paper());
+        //items.add(new Maze());
+        items.add(new SimpleButton());
+        items.add(new SimpleButtonBordered());
+        items.add(new Key(new Point(x, y)));
+        items.add(new ColorCombo(new Point(x, y)));
+        items.add(new NamedButton("NamedButton", s, new Point(x, y)));
+        items.add(new NumberedDoor("NumberedDoor", s, new Point(x, y)));
+        //items.add(new RushHourPuzzle());
+        for (int counter = 0; counter < items.size(); counter++) {
+            buttonOver.add(false);
+            buttons.add(new Rectangle(0, counter * 20, 120, 20));
+        }
+    }
+
+    public Graphics drawEditor(Graphics g, ImageObserver io, int mouseX, int mouseY) {
+        x = mouseX + xoffset;
+        y = mouseY + yoffset;
+        drawMenu(g);
+        drawItems(g, io);
+        if (r != null) {
+            r.draw(g, io);
+        }
+        return g;
+    }
+
+    public Graphics drawItems(Graphics g, ImageObserver io) {
+        for (Item i : placed) {
+            i.draw(g, io);
+        }
+        if (!menu) {
+            selected.set(x, y);
+            selected.draw(g, io);
+        }
+        return g;
+    }
+
+    public Graphics drawMenu(Graphics g) {
+        if (menu) {
+            for (int counter = 0; counter < items.size(); counter++) {
+                if (buttons.get(counter).contains(x, y)) {
+                    buttonOver.set(counter, true);
+                    g.setColor(Color.YELLOW);
+                    fillRect(buttons.get(counter), g);
+                    g.setColor(Color.BLACK);
+                } else {
+                    buttonOver.set(counter, false);
+                }
+                g.drawString(items.get(counter).getName() + " ", 5, (counter * 20) + 15);
+                drawRect(buttons.get(counter), g);
+            }
+        }
+        return g;
+    }
+
+    public void click() {
+        if (menu) {
+            for (int counter = 0; counter < items.size(); counter++) {
+                if (buttonOver.get(counter)) {
+                    //System.out.println("clicked");
+                    //items.get(counter).
+                    selected = items.get(counter);
+                    menu = !menu;
+                }
+            }
+        } else {
+            placed.add(selected);
+            menu = !menu;
+            xoffset = 0;
+            yoffset = 0;
+            //System.out.println(selected);
+        }
+    }
+
+    public void offset(boolean left, boolean right, boolean up, boolean down) {
+        if (left) {
+            xoffset--;
+        }
+        if (right) {
+            xoffset++;
+        }
+        if (up) {
+            yoffset--;
+        }
+        if (down) {
+            yoffset++;
+        }
+    }
+
+    public Graphics drawRect(Rectangle r, Graphics g) {
+        g.drawRect((int) r.getX(), (int) r.getY(), (int) r.getWidth(), (int) r.getHeight());
+        return g;
+    }
+
+    public Graphics fillRect(Rectangle r, Graphics g) {
+        g.fillRect((int) r.getX(), (int) r.getY(), (int) r.getWidth(), (int) r.getHeight());
+        return g;
+    }
+}
